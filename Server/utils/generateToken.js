0d69a5fd6af506df1b09fb9de6854b784fab11eb
Shubham Bihade <shubhambihade24@gmail.com>
2026-06-08 19:14:25 +0530

    axios changes new

--- Server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return jwt.sign({ userId: id }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-};
-
-module.exports = generateToken;
